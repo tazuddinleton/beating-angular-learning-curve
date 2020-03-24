7139751f670255ac05e3f43d08f9d1674afbbbb6
Taz Uddin <getleton@gmail.com>
2020-03-24 08:25:50 +0600

    added test case

--- es6fundamentals/src/functional_programming/arrows_and_async.spec.js
+++ es6fundamentals/src/functional_programming/arrows_and_async.spec.js
@@ -1,11 +1,13 @@
-describe("arrow function and async",()=>{
+describe("arrow function and async/callback",()=>{
     it("lexically binds to this", (done)=>{
         this.name = "Bob";
         
         // using traditional syntax
+        let self = this;
         setTimeout(function(){
-            // this.name is undefined here
+            // this.name is undefined here            
             expect(this.name).toBe(undefined);    
+            expect(self.name).toBe("Bob");
             done();
         }, 15);
 
