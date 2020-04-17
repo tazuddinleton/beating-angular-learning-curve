4afa938358b25e7d178c0d679c338bcbe36e9ea8
Taz Uddin <getleton@gmail.com>
2020-04-17 19:41:36 +0600

    classess interfaces and extends, implements

---
+++ typescript-in-depth/basics/src/classes_and_interfaces.ts
@@ -0,0 +1,86 @@
+export interface Person {
+  name: string;
+  email: string;
+  occupation?: string;
+  logDetail? : () => void;
+}
+
+export interface Employee extends Person {
+  designation: string;
+}
+
+export interface Book {
+    title: string;
+    pubDate: Date;    
+}
+
+export interface Author extends Person {
+    publications: Book[]
+}
+
+
+
+export class Architect implements Employee {
+    designation: string;
+    name: string;
+    email: string;    
+    occupation: string;
+    constructor(){
+        this.designation = "Architect";
+        this.name = "John Doe",
+        this.email = "j@d";        
+        this.occupation = this.designation;
+    }
+    
+}
+
+export class Developer implements Employee {
+    designation: string;
+    name: string;
+    email: string;    
+    occupation: string;
+    constructor(){
+        this.designation = "Developer";
+        this.name = "Robin Hood",
+        this.email = "r@h"
+        this.occupation = this.designation;
+    }    
+}
+
+export class FictionAuthor implements Author {
+    publications: any[];
+    name: string;
+    email: string;
+    occupation: string;
+    constructor(){
+        this.name = "JK Rowling",
+        this.email = "jk@r"
+        this.publications = [{
+            title: "Harry Potter and the Goblet of Fire",
+            pubDate: new Date()
+        }];
+        this.occupation = "Fiction author";
+    }
+    
+}
+
+export function logPerson(person: Person){
+    console.log(`Name: ${person.name} is ${person.occupation}, can be reached at ${person.email}`);
+}
+
+export function logAuthor(author: Author){
+    logPerson(author);
+    console.log("Books published: ");
+    author.publications.forEach(book => console.log(`Title: ${book.title}, Puplished at: ${book.pubDate.toLocaleDateString()})`));
+}
+
+export function logEmployee(employee: Employee){
+    logPerson(employee);    
+}
+
+logEmployee(new Developer());
+logEmployee(new Architect());
+logAuthor(new FictionAuthor());
+
+
+console.log();
