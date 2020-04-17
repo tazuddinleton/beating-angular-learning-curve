c3ee06ea8e79f139fa3acb51c1f09817280bbe9f
Taz Uddin <getleton@gmail.com>
2020-04-17 21:26:19 +0600

    static properties and methods

---
+++ typescript-in-depth/basics/src/static_properties.ts
@@ -0,0 +1,30 @@
+export class Person {    
+    constructor(public name: string){
+        Person.instances ++;        
+    }  
+    static instances: number = 0;
+    static getInstanceCount():number{
+        return Person.instances;
+    }
+}
+
+
+
+let p1 = new Person("Person 1");
+let p2 = new Person("Person 2");
+
+console.log(Person.instances);
+
+console.log(Person.getInstanceCount());
+
+
+
+
+
+
+
+
+
+
+
+
