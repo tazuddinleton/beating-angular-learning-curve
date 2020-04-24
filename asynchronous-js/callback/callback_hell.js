45111e3ef9a68d95a9a4147852c02ef2ead4e39f
= <=>
2020-04-24 19:43:29 +0600

    added some comments

--- asynchronous-js/callback/callback_hell.js
+++ asynchronous-js/callback/callback_hell.js
@@ -1,3 +1,34 @@
+/**
+ * Callback hell is  
+ * the deficiencies associated with 
+ * the callback only programming style. 
+ * Where you have to deal with two most difficult
+ * problem of async programming one of them 
+ * being the Inversion of Control
+ * another one is the Non-local Non-sequential reasoning.
+ * 
+ * Inversion of Control: By using callback you giveawa your
+ * capability of controlling your code the way you want. It can
+ * introduce unexpected errors into your code.
+ * 
+ * Non-local Non-sequeential reasoning aspect is the complexity you 
+ * introduce into your code by nesting more and more callbacks to 
+ * fulfill the desired behavior of your code. The human brain is 
+ * fundamentally single threaded and therefore it's difficult for
+ * humans to reason asynchronously written code.
+ * 
+ * To address these issues we need higher order abstractions 
+ * like Promises, Generators, Thunks etc.
+ * 
+ * Promises are really a placeholder for a future value that will be 
+ * available upon completion of the request. In this sense of promise
+ * a promise gives you an event listener where you can subscribe and 
+ * get notified when your value is ready to harness.
+ * Promises helps deal with trust issue by resolving only once.
+ *  
+ *  
+ */
+
 function fakeAjax(url, cb) {
     var fake_responses = {
         "file1": "The first text",
