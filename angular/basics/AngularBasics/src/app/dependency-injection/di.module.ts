90da1f21f141762d7294341eac2972fa7dd6fb43
Taz Uddin <getleton@gmail.com>
2020-07-02 20:58:01 +0600

    Add reference to declare keyword

--- angular/basics/AngularBasics/src/app/dependency-injection/di.module.ts
+++ angular/basics/AngularBasics/src/app/dependency-injection/di.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { DiComponent } from './di.component';
 import { JS_STRING_SVC, IJsStringService } from './my-js-string-service';
 
+//Reference: https://dzone.com/articles/quick-tip-%E2%80%93-typescript-declare#:~:text=The%20declare%20keyword%20is%20used,myLibrary%20in%20the%20global%20namespace.
 declare let stringService: IJsStringService;
 
 @NgModule({
